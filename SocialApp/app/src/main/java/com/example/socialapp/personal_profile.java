cc1f7cba8bffe27c165c03e0558b08923de2c739
Kamingos <gooaal.j@mail.ru>
2023-04-11 22:38:22 +0700

    day1

---
+++ SocialApp/app/src/main/java/com/example/socialapp/personal_profile.java
@@ -0,0 +1,13 @@
+package com.example.socialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class personal_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_profile);
+    }
+}
